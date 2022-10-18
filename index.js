4799960a963904eeaa5d9169e7f6984f39a31ff3
Wansoft <wansoft.developer@gmail.com>
2022-10-17 20:55:22 -0500

    Hoisting JS

---
+++ index.js
@@ -0,0 +1,8 @@
+var resultado1 = sumar(numero1, numero2);
+var numero1 = 4;
+var numero2 = 5;
+var resultado2 = sumar(10, 5)
+
+function sumar(num1, num2) {
+    return num1 + num2
+}
---
+++ persona.js
@@ -0,0 +1,8 @@
+export class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
+
+
